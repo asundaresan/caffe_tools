d225d9ca21036527aa7e8eaaf86071454dbcb125
Aravind Sundaresan <asundaresan@gmail.com>
2017-01-18 13:51:58 -0800

    Add compare to plot_learning_curve.py

--- examples/cats_and_dogs/scripts/plot_learning_curve.py
+++ examples/cats_and_dogs/scripts/plot_learning_curve.py
@@ -14,36 +14,78 @@ def to_dict( dict_list ):
   return { k: list( d[k] for d in dict_list ) for k in dict_list[0].keys() }
 
 
-def main( logfilename, verbose = 0 ):
+def get_plots( logfile, verbose = 0 ):
   if verbose > 0:
-    print( "Loading %s" % logfilename )
-  train_dict_list, test_dict_list = parse_log( logfilename )
+    print( "Loading %s" % logfile )
+  train_dict_list, test_dict_list = parse_log( logfile )
   train_dict = to_dict( train_dict_list )
   test_dict = to_dict( test_dict_list )
   if verbose > 0:
     print( test_dict.keys() )
-  tr_loss, = plt.plot( train_dict["NumIters"], np.array( train_dict["loss"] ), label = "Training loss" )
-  te_loss, = plt.plot( test_dict["NumIters"], np.array( test_dict["loss"] ), label = "Test loss" )
-  te_acc, = plt.plot( test_dict["NumIters"], test_dict["accuracy"], label = "Test accuracy" )
-  plt.legend( handles = [tr_loss, te_loss, te_acc] )
-  plt.show( block = False )
-  path, extension = os.path.splitext( logfilename )
-  pngfilename = "%s.png" % path
+  plots = {}
+  plots["Test accuracy"] = { "xval": test_dict["NumIters"], "yval": test_dict["accuracy"] }
+  plots["Test loss"] = { "xval": test_dict["NumIters"], "yval": test_dict["loss"] }
+  plots["Train loss"] = { "xval": train_dict["NumIters"], "yval": train_dict["loss"] }
+  return plots
+
+
+
+def plot_log( logfile, block = True, verbose = 0 ):
+  handles = []
+  plots = get_plots( logfile, verbose = verbose )
+  for key, plot in plots.items():
+    h, = plt.plot( plot["xval"], plot["yval"], label = key )
+    handles.append( h )
+
+  plt.legend( handles = handles, bbox_to_anchor=(0.01, 0.01), loc=3 )
+  plt.grid( True )
+  axes = plt.gca()
+  axes.set_ylim( [0,1] )
+
+  path, extension = os.path.splitext( logfile )
+  outputfile = "%s.png" % path
+  if verbose > 0:
+    print( "Saving to file %s" % logfile )
+  plt.savefig( outputfile )
+  plt.show( block = block )
+
+
+def compare_logs( logfile_list, block = True, verbose = 0 ):
+  handles = []
+  for logfile in logfile_list:
+    path, extension = os.path.splitext( logfile )
+    name = os.path.basename( path )
+    plots = get_plots( logfile, verbose = verbose )
+    for key, plot in plots.items():
+      if "accuracy" in key:
+        print( "%s: max accuracy = %.3f" % ( name, max( plot[ "yval" ] ) ) )
+        h, = plt.plot( plot["xval"], plot["yval"], label = name )
+        handles.append( h )
+  plt.legend( handles = handles, bbox_to_anchor=(0.01, 0.01), loc=3 )
+  plt.grid( True )
+  axes = plt.gca()
+  axes.set_ylim( [0,1] )
+
+  outputfile = "compare.png"
   if verbose > 0:
     print( "Saving to file %s" % logfilename )
-  plt.savefig( pngfilename )
+  plt.savefig( outputfile )
+  plt.show( block = block )
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument( "log_file", help = "Log filename folder" )
+  parser.add_argument( "log_files", nargs = "*", help = "Log filename folder" )
   parser.add_argument( "--continuous", "-C", action="store_true", help="Plot continuously" )
   parser.add_argument( "--verbose", "-v", action="count", default = 0, help="Verbosity level" )
   args = parser.parse_args()
 
   if args.continuous:
     while 1:
-      main( args.log_file )
+      plot_log( args.log_files, verbose = args.verbose )
       time.sleep( 1 )
-
-  main( args.log_file )
+  else:
+    if len( args.log_files ) > 1:
+      compare_logs( args.log_files )
+    elif len( args.log_files ) == 1:
+      plot_log( args.log_files[0] )
