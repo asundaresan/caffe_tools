38396919955734c6989d98f388cddef9e0042179
Aravind Sundaresan <asundaresan@gmail.com>
2017-01-19 10:19:08 -0800

    Update caffe_model_2 and add run.sh

---
+++ examples/cats_and_dogs/models/caffe_model_2/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+TRIALS=10
+for (( c=1; c<=${TRIALS}; c++ )); do 
+  echo "== Trial $c =="
+  python ../../scripts/create_lmdb.py data/images data/lmdb -S ${c}
+  compute_image_mean -backend=lmdb data/lmdb/train_lmdb data/mean.binaryproto
+  caffe train --solver solver_2.prototxt 2>&1 | tee model_train_seed-${c}.log
+  echo "== with -A option =="
+  python ../../scripts/create_lmdb.py data/images data/lmdb -S ${c} -A
+  compute_image_mean -backend=lmdb data/lmdb/train_lmdb data/mean.binaryproto
+  caffe train --solver solver_2.prototxt 2>&1 | tee model_train_A_seed-${c}.log
+done
